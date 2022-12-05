b602b819050374e42f589defd2301555821d9fa2
Jasper <wilsonlee0730@gmail.com>
2022-12-05 19:36:16 +0800

    2022.12.05

--- src/js/index.js
+++ src/js/index.js
@@ -5,14 +5,16 @@ import 'overlayscrollbars/css/OverlayScrollbars.min.css';
 Modal4.defineMethods({
   useOpen(modal) {
     const targetModal = $(modal)
-    const target = targetModal.find('.item')[5]
+    const target = targetModal.find('.item')[1]
     const scroller = targetModal.find('.os-viewport')[0];
     const modalAost = new Aost4('modern-modal[data-modal-id="project-modal"] [data-aost]', {
       scroller: scroller,
     });
 
     Anchor4.run({
-      target,
+      target: target,
+      container: scroller,
+      gap: 100,
       on: {
         afterScroll() {
           alert('我只是say個hi')
