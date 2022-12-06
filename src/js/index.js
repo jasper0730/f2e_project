664110ded39bae3e8a013e743859ac92ad89477f
jasper <zhengxian0730@gmail.com>
2022-12-07 03:17:50 +0800

    2022.12.07

--- src/js/index.js
+++ src/js/index.js
@@ -2,39 +2,71 @@
 import OverlayScrollbars from 'overlayscrollbars';
 import 'overlayscrollbars/css/OverlayScrollbars.min.css';
 
-const index = {}
+const plugins = {}
 
-Modal4.defineMethods({
-  useOpen(modal) {
-    const targetModal = $(modal)
-    const target = targetModal.find('.block')[1]
-    const scroller = targetModal.find('.os-viewport')[0];
-    const plugin = {
-      modalAost: new Aost4('modern-modal[data-modal-id="project-modal"] [data-aost]', {
-        scroller: scroller,
-      }),
-      modalVideo: new Video4('[video-target]'),
-      categorySlider(selector, options) {
-        new CategorySlider(selector, options);
-      },
+plugins.modal4 = function () {
+  Modal4.defineMethods({
+    useOpen(modal) {
+      const targetModal = $(modal)
+      const target = targetModal.find('.block')[1]
+      const scroller = targetModal.find('.os-viewport')[0];
+      const plugin = {
+        modalAost: new Aost4('modern-modal[data-modal-id="project-modal"] [data-aost]', {
+          scroller: scroller,
+        }),
+      }
+      plugins.anchor4(target, scroller)
+      plugins.imagePreview('.upload-btn')
     }
+  })
+}
 
-    // Anchor4.run({
-    //   target: target,
-    //   container: scroller,
-    //   gap: 150,
-    //   on: {
-    //     afterScroll() {
-    //       alert('我只是say個hi')
-    //     }
-    //   }
-    // })
+plugins.imagePreview = function (target) {
+  const option = {
+    on: {
+      changeAfter() {
+        alert('success')
+      },
+      overLimit() {
+        alert('error')
+      }
+    },
+    sizeLimit: 1
   }
+  _g.imagePreview(target, option);
+}
+
+plugins.anchor4 = function (target, scroller) {
+  Anchor4.run({
+    target: target,
+    container: scroller,
+    gap: 150,
+    on: {
+      afterScroll() {
+        alert('我只是say個hi')
+      }
+    }
+  })
+}
+
+plugins.categorySlider = function () {
+  const option = {
+    breakpoint: 1200
+  }
+  _g.categorySlider('.category-slider', option);
+}
+
+plugins.all = function () {
+  this.modal4();
+  this.categorySlider();
+  this.imagePreview();
+}
+
+
+
+
+
+$(() => {
+  plugins.all()
 })
-
-
-
-
-
-$(() => { })
 
